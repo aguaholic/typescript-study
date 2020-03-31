eb0d21b9dbaac1d2faded1f01559f7e77b06406f
aguaholic <joyterra@gmail.com>
2020-03-31 16:32:33 +0200

    Implement Drag and Drop utilizing interfaes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
+// Drag & Drop Interfaces
+interface Draggable {
+  dragStartHandler(event: DragEvent): void
+  dragEndHandler(event: DragEvent): void
+}
+
+interface DragTarget {
+  dragOverHandler(event: DragEvent): void
+  dropHandler(event: DragEvent): void
+  dragLeaveHandler(event: DragEvent): void
+}
+
 // Project Type
 enum ProjectStatus { Active, Finished }
 
 class Project {
     constructor(
-        public id: string, 
-        public title: string, 
-        public description: string, 
-        public people: number, 
-        public status: ProjectStatus
+      public id: string, 
+      public title: string, 
+      public description: string, 
+      public people: number, 
+      public status: ProjectStatus
     ) {}
 }
 
@@ -152,10 +164,17 @@ abstract class Component <T extends HTMLElement , U extends HTMLElement>{
 }
 
 // ProjectItem Class
-
-class ProjectItem extends Component <HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component <HTMLUListElement, HTMLLIElement>
+  implements Draggable{
   private project: Project
 
+  get persons() {
+    if (this.project.people === 1) {
+      return '1 person'
+    } else {
+      return `${this.project.people} persons` 
+    }
+  }
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id)
     this.project = project
@@ -164,10 +183,23 @@ class ProjectItem extends Component <HTMLUListElement, HTMLLIElement> {
     this.renderContent()
   }
 
-  configure() {}
+  @autobind
+  dragStartHandler(event: DragEvent) {
+    console.log(event)
+  }
+
+  dragEndHandler(_: DragEvent) {
+    console.log('DragEnd')
+  }
+
+  configure() {
+    this.element.addEventListener('dragstart', this.dragStartHandler)
+    this.element.addEventListener('dragstart', this.dragEndHandler)
+  }
+
   renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title
-    this.element.querySelector('h3')!.textContent = this.project.people.toString()
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
     this.element.querySelector('p')!.textContent = this.project.description
   }
 }
